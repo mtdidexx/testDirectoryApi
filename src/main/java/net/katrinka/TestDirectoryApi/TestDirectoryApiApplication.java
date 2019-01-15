6040b338f1ec1f2641f18070a304b8b0ea24478c
Mark Donahue <mark-donahue@idexx.com>
2019-01-15 16:01:56 -0500

    Import files from start.spring.io

---
+++ src/main/java/net/katrinka/TestDirectoryApi/TestDirectoryApiApplication.java
@@ -0,0 +1,14 @@
+package net.katrinka.TestDirectoryApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDirectoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDirectoryApiApplication.class, args);
+	}
+
+}
+
